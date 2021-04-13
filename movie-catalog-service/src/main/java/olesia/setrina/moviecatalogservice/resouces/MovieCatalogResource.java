46caa3bfb08b268e48be8c1d1b8158b9c3cbf7f9
Setrina Olesia <setrina527@gmail.com>
2021-04-13 19:59:31 +0300

    add discovery server Eureka

--- movie-catalog-service/src/main/java/olesia/setrina/moviecatalogservice/resouces/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/olesia/setrina/moviecatalogservice/resouces/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package olesia.setrina.moviecatalogservice.resouces;
 import olesia.setrina.moviecatalogservice.models.CatalogItem;
 import olesia.setrina.moviecatalogservice.models.Movie;
 import olesia.setrina.moviecatalogservice.models.Rating;
+import olesia.setrina.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,24 +27,18 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
-        return ratings.stream().map(rating -> {
+// For each movie ID, call movie info service and get details
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        return ratings.getUserRating().stream().map(rating -> {
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-
+            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+        })
+                .collect(Collectors.toList());
+    }
+}
 //            Movie movie = webClientBuilder.build()
 //                    .get()
 //                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
 //                    .retrieve()
 //                    .bodyToMono(Movie.class)
 //                    .block();
-
-            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
-        })
-                .collect(Collectors.toList());
-
-    }
-}
--- ratings-data-service/src/main/java/olesia/setrina/ratingsdataservice/resources/RatingsDataResource.java
+++ ratings-data-service/src/main/java/olesia/setrina/ratingsdataservice/resources/RatingsDataResource.java
@@ -1,6 +1,7 @@
 package olesia.setrina.ratingsdataservice.resources;
 
 import olesia.setrina.ratingsdataservice.models.Rating;
+import olesia.setrina.ratingsdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +14,18 @@ import java.util.List;
 public class RatingsDataResource {
 
     @RequestMapping("/{movieId}")
-    public Rating getRating(@PathVariable("movieId") String movieId){
-        return  new Rating(movieId, 4);
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
     }
 
     @RequestMapping("/users/{userId}")
-    public List<Rating> getUserRating(@PathVariable("userId") String userId){
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234", 4),
                 new Rating("5678", 3)
         );
-        return  ratings;
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 }
